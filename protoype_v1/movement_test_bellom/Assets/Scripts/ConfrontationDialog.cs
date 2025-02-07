4379288dc0631009f76a6df6283aaacc33d61bf5
musak6191 <musa.k123@outlook.de>
2025-02-07 19:05:05 +0100

    Level3 Confrontation gecoded
    Abschließenden Dialog mit Monster

--- protoype_v1/movement_test_bellom/Assets/Scripts/ConfrontationDialog.cs
+++ protoype_v1/movement_test_bellom/Assets/Scripts/ConfrontationDialog.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ConfrontationDialog : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int dialogsenTextIndex = 0;
+    public TextMeshProUGUI uiDialogInfo;
+    public TextMeshProUGUI MagazineInfo;
+    public PlayerMovement playerMovement;
+    List<string> ConfrontationNPC = new List<string> { "You slaughtered my people.",
+                                                   "My friends.",
+                                                    "My family.",
+                                                    "And yet",
+                                                    "you call us the monsters.",
+                                                    "...",
+                                                    "Noone will miss you."};
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dialogsenTextIndex < (ConfrontationNPC.Count - 1))
+            {
+                NextDialogElement();
+            }
+            else
+            {
+                EndDialog();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerMovement.playerDialogActive = true;
+        StartDialog();
     }
 
-    // Update is called once per frame
-    void Update()
+    void StartDialog()
     {
-        
+        dialogsenTextIndex = 0;
+        uiDialogInfo.text = ConfrontationNPC[dialogsenTextIndex];
+        MagazineInfo.rectTransform.anchoredPosition += new Vector2(0, 67.5f);
+    }
+
+    void NextDialogElement()
+    {
+        dialogsenTextIndex += 1;
+        uiDialogInfo.text = ConfrontationNPC[dialogsenTextIndex];
+    }
+
+    void EndDialog()
+    {
+        MagazineInfo.rectTransform.anchoredPosition -= new Vector2(0, 67.5f);
     }
 }
