852e1169c6f442d0f9349f5fca90d28b26ba7a59
alexismaras <alexis.maras@live.de>
2025-02-10 18:02:59 +0100

    Nachtrag

---
+++ protoype_v1/movement_test_bellom/Assets/Scripts/AssetSort.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AssetSort : MonoBehaviour
+{
+    [SerializeField] PlayerMovement playerMovement; 
+    [SerializeField] GameObject playerSprite; 
+    SpriteRenderer spriteRenderer;
+
+    SpriteRenderer playerSpriteRenderer;
+
+    float spriteOffset; 
+    float playerOffset;
+    float playerHeight;
+    float spriteHeight;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer playerSpriteRenderer = playerSprite.GetComponent<SpriteRenderer>();
+        spriteHeight = spriteRenderer.bounds.size.y;
+        spriteOffset = spriteHeight * 0.5f;
+        playerHeight = playerSpriteRenderer.bounds.size.y;
+        playerOffset = playerHeight * 0.5f;
+    }
+
+    void Update()
+    {
+
+        if (playerMovement.transform.position.y - playerOffset < transform.position.y - spriteOffset)
+        {
+            spriteRenderer.sortingLayerName = "WallBehindPlayer";
+            // spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * -100) - 1;
+        }
+        else
+        {
+            // spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * -100) + 1;
+            spriteRenderer.sortingLayerName = "WallBeforePlayer";
+        }
+    }
+}
